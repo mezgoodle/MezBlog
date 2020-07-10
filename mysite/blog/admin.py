aa9b1cf1d03b04860f2beca08fe8d574b4c4b52b
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-10 15:37:56 +0300

    Registrate on admin panel

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ('status',)
-    search_fields = ['title', 'content']
+    search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
 
-admin.site.register(Post, PostAdmin)
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
