0d76c109df21fdd2d0196d9671a074345f790a08
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-10 17:39:29 +0300

    Update forms.py

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,8 +1,22 @@
 from .models import Comment
-from django import forms
+from django.forms import ModelForm, TextInput, Textarea, EmailInput
 
 
-class CommentForm(forms.ModelForm):
+class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ('author', 'email', 'body')
+        widgets = {
+            'author': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter name',
+            }),
+            'email': EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter your email',
+            }),
+            'body': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter comment',
+            }),
+        }
