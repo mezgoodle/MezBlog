a72d1694e5e07451f2e90d54472a47ee8bca89c2
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-10 17:19:26 +0300

    Edit views

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail')
+    path('', views.PostList, name='home'),
+    path('<slug:slug>/', views.PostDetail, name='post_detail')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,29 @@
-from django.views import generic
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+def PostList(request):
+    posts = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+    return render(request, template_name, {'post_list': posts})
 
 
-class PostDetail(generic.DetailView):
-    model = Post
+def PostDetail(request, slug):
     template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render (request, template_name, {'post': post,
+                                            'comments': comments,
+                                            'new_comment': new_comment,
+                                            'comment_form': comment_form})
