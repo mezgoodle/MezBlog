15f60a70daff32f7fe61624ea0feb13f76583d64
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-11 10:35:03 +0300

    Update urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
     path('', include('blog.urls')),
 ]
