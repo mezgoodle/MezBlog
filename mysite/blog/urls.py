e78698bbd9e66917fdddb3b4f9402bab39eb6e74
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-14 10:38:18 +0300

    Update urls.py

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .feeds import LatestPostsFeed, AtomSiteNewsFeed
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.PostList, name='home'),
     path('<slug:slug>/', views.PostDetail, name='post_detail'),
