c50715e0ad370c588e3b344a8a5b292e46e11b33
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-07-17 21:34:23 +0300

    Set url

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,6 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostList, name='home'),
     path('<slug:slug>/', views.PostDetail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('feed/rss', LatestPostsFeed(), name='post_feed'),
     path('feed/atom', AtomSiteNewsFeed()),
 ]
